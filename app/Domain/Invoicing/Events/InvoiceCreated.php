1d6a74f478970b33bc071558e61220d7e7fae591
jaddoueik1 <65397044+jaddoueik1@users.noreply.github.com>
2025-11-21 01:13:23 +0200

    Add missing domain event classes

---
+++ app/Domain/Invoicing/Events/InvoiceCreated.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Domain\Invoicing\Events;
+
+use App\Core\Events\DomainEvent;
+
+class InvoiceCreated extends DomainEvent
+{
+}
---
+++ app/Domain/Visits/Events/VisitCreated.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Domain\Visits\Events;
+
+use App\Core\Events\DomainEvent;
+
+class VisitCreated extends DomainEvent
+{
+}
