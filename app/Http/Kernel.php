081542a8b6ae95ca7e63d6b773fa8e3ecdae2b7a
Jad Doueik <jadtamerdoueik@gmail.com>
2025-11-21 10:26:08 +0200

    fixes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -51,7 +51,6 @@ class Kernel extends HttpKernel
 
         'api' => [
             EnsureFrontendRequestsAreStateful::class,
-            'throttle:api',
             SubstituteBindings::class,
         ],
     ];
